2743d4be3416d02fcbc6b1836223af0c920c6ac2
Meer Faridul Haque <meerhaque@gmail.com>
2022-05-30 17:46:55 +0600

    udpate

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,43 +1,42 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 5,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    breakpoints: {
-        300: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-        },
-        1200: {
-            slidesPerView: 5,
-            spaceBetween: 30,
-        },
-
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
     },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+  },
 });
 
 var swiper3 = new Swiper(".mySwiper3", {
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 1,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
-    loop: true,
-    spaceBetween: 0,
-    // navigation: {
-    //   nextEl: ".swiper-button-next",
-    //   prevEl: ".swiper-button-prev",
-    // },
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 1,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 0,
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  thumbs: {
+    swiper: swiper,
+  },
+});
