7d81e16cf8e934567abfdf2bcb498bdc50926d72
meersajib <meerhaque@gmail.com>
2022-06-02 18:03:24 +0600

    update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -23,13 +23,7 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
-var swiper3 = new Swiper(".mySwiper3", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 1,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
+
 var swiper2 = new Swiper(".mySwiper2", {
   loop: true,
   spaceBetween: 0,
@@ -40,4 +34,51 @@ var swiper2 = new Swiper(".mySwiper2", {
   thumbs: {
     swiper: swiper,
   },
+});
+
+var swiper = new Swiper(".relatedProducts", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 3,
+  freeMode: true,
+  watchSlidesProgress: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Close cart sidebar after clicking body
+$(document).ready(function () {
+  $(".cart-wrapper").click(function (e) {
+    e.stopPropagation();
+    $(".collapse-horizontal").addClass("show");
+
+  });
+  $("body").click(function () {
+    $(".collapse-horizontal").removeClass("show");
+  });
+  $(".close-cart").click(function (e) {
+    e.stopPropagation();
+    $(".collapse-horizontal").removeClass("show");
+  });
+});
+// Hide searchbar when scroll
+$(window).scroll(function () {
+  console.log('scrolling')
+
+  if ($(this).scrollTop() > 50) {
+    $('.search-field').addClass('hide-search-field');
+    $('.search-icon-container').removeClass('hide-search-field');
+  } else {
+    $('.search-field').removeClass('hide-search-field');
+    $('.search-icon-container').addClass('hide-search-field');
+  }
+});
+
+$(function () {
+  $(".search-icon-container").click(function () {
+    $(this).addClass("hide-search-field");
+    $('.search-field').removeClass('hide-search-field');
+  });
 });
